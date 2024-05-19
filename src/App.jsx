d27f55b50da93836c09b57d847194ea415df9ed2
somveergithub <somveersingh24121998@gmail.com>
2024-05-19 14:02:50 +0530

    't'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import heartLogo from '/assets/red_heart.svg'
 import './App.css'
 
 function App() {
