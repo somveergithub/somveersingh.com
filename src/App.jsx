e6c18b9f3de59f5737410e71c8ff306889480fd6
somveergithub <somveersingh24121998@gmail.com>
2024-06-01 20:17:37 +0530

    reverting

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="heading">Color Changer</h1>
-      <span className="button" id="Grey"></span>
-      <span className="button" id="White"></span>
-      <span className="button" id="Blue"></span>
-      <span className="button" id="Yellow"></span>
-      <h2>Try Clicking on one of the colors above to change the background-color of this page!</h2>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Somveer Singh</h1>
     </>
   )
 }
-
-const body = document.querySelector('body')
-console.log(body);
-
-const buttons = document.querySelectorAll('.button')
-console.log(buttons);
-
-function setButtonsBorder(buttons, color) {
-  buttons.forEach(function (button) {
-    button.style.border = `solid ${color} 2px`
-  })
-}
-
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log('target: ', e.target);
-    const id = e.target.id
-    console.log(id);
-
-    switch (id) {
-      case "Grey":
-        body.style.backgroundColor = id
-        body.style.color = "white"
-        setButtonsBorder(buttons, 'black')
-        break;
-      case "White":
-        body.style.backgroundColor = id
-        body.style.color = "black"
-        setButtonsBorder(buttons, 'black')
-        break;
-      case "Blue":
-        body.style.backgroundColor = id
-        body.style.color = "white"
-        setButtonsBorder(buttons, 'Red')
-        break;
-      case "Yellow":
-        body.style.backgroundColor = id
-        body.style.color = "Red"
-        setButtonsBorder(buttons, 'Blue')
-        break;
-      default:
-        body.style.backgroundColor = "black"
-        body.style.color = "white"
-        setButtonsBorder(buttons, 'black')
-    }
-  })
-});
 
 export default App
